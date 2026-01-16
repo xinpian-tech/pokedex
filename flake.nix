ff1a704094612677c4ef6f627fac808d50f6276d
Avimitin <dev@avimit.in>
2026-01-16 16:29:47 +0800

    pokedex: fix nix build and add justfile invocation
    Signed-off-by: Avimitin <dev@avimit.in>

---
+++ flake.nix
@@ -0,0 +1,77 @@
+{
+  description = "ASL RISC-V Golden Model";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      treefmt-nix,
+      ...
+    }:
+    let
+      overlay = import ./nix/overlay.nix;
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      # Add supported platform here
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      flake = {
+        overlays = {
+          default = overlay;
+        };
+      };
+
+      imports = [
+        # Add treefmt flake module to automatically configure and add formatter to this flake
+        treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        { system, ... }:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              overlay
+            ];
+          };
+        in
+        {
+          _module.args.pkgs = pkgs;
+          legacyPackages = pkgs;
+
+          devShells = {
+            default = pkgs.mkShell {
+              buildInputs = [
+                pkgs.just
+              ];
+            };
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            settings.on-unmatched = "debug";
+            programs = {
+              nixfmt.enable = true;
+              black.enable = true;
+
+              # treefmt-nix can not determine edition automatically,
+              # unlike 'cargo fmt' which reads from Cargo.toml.
+              #
+              # rustfmt.enable = true;
+            };
+          };
+        };
+    };
+}
